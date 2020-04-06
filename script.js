d63194527c73d002bd1564ef708c018bfce011e8
alexRhine22 <alextherhine@gmail.com>
2020-04-06 16:15:59 -0700

    Created js function to add new book input to table

--- script.js
+++ script.js
@@ -3,15 +3,6 @@ const bookForm = document.getElementById("book-form");
 const newBookButton = document.getElementById("new-book-button");
 const bookTable = document.getElementById("book-table");
 
-/**
- * eventlistener for add new book button
- */
-bookFormButton.addEventListener('click', (e) => {
-    e.preventDefault(); 
-
-    bookForm.classList.toggle('active');
-});
-
 let myLibrary = [];
 
 function Book(
@@ -27,15 +18,35 @@ function Book(
 }
 
 function addBookToLibrary() {
+    var titleInput = document.getElementById('book-title').value;
+    var authorInput = document.getElementById('book-author').value;
+    var pagesInput = document.getElementById('book-pages').value;
 
+    var newBook = new Book(titleInput, authorInput, pagesInput);
+
+    myLibrary.push(newBook);
+    
+    var row = bookTable.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);        
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = myLibrary[myLibrary.length - 1].bookTitle;
+    cell2.innerHTML = myLibrary[myLibrary.length - 1].bookAuthor;
+    cell3.innerHTML = myLibrary[myLibrary.length - 1].bookPages;
 }
 
-// function myFunction() {
-//     var row = bookTable.insertRow(-1);
-//     var cell1 = row.insertCell(0);
-//     var cell2 = row.insertCell(1);
-//     cell1.innerHTML = "NEW CELL1";
-//     cell2.innerHTML = "NEW CELL2";
-// }
+/**
+ * eventlistener for add new book button
+ */
+bookFormButton.addEventListener('click', (e) => {
+    e.preventDefault(); 
 
-// myFunction();
+    bookForm.classList.toggle('active');
+});
+
+newBookButton.addEventListener('click', (e) => {
+    e.preventDefault(); 
+
+    addBookToLibrary();
+    bookForm.classList.toggle('active');
+});
