119b1ef5130d3b1f3926ef494f645a941920d97a
alexRhine22 <alextherhine@gmail.com>
2020-04-07 10:37:48 -0700

    Added radio button valdiation to js

--- script.js
+++ script.js
@@ -5,31 +5,49 @@ const bookTable = document.getElementById('book-table');
 
 let myLibrary = [];
 
+/**
+ * Book object constructor
+ * @param {*} title 
+ * @param {*} author 
+ * @param {*} pages 
+ */
 function Book(
     title,
     author,
     pages,
-    // read
+    read
 ) {
     this.bookTitle = title;
     this.bookAuthor = author;
     this.bookPages = pages;
-    // this.bookRead = read;
+    this.bookRead = read;
 }
 
+/**
+ * adds new book object to my library array
+ */
 function addBookToLibrary() {
     var titleInput = document.getElementById('book-title').value;
     var authorInput = document.getElementById('book-author').value;
     var pagesInput = document.getElementById('book-pages').value;
+    var bookRead = document.getElementById('book-read-yes').checked;
+    var bookNotRead = document.getElementById('book-read-no').checked;
 
-    console.log(titleInput);// for checking input
-    console.log(authorInput);  // for checking input
-    console.log(pagesInput);  // for checking input
+    // console.log(titleInput);
+    // console.log(authorInput);  
+    // console.log(pagesInput);  
+    // console.log(bookRead);
+    // console.log(bookNotRead);
 
-    if (titleInput != '' || authorInput != '' || pagesInput != '') {
+    if (titleInput == '' || authorInput == '' || pagesInput == '') {
         alert('Please fill out every field to add a book to the database.');
     } else {
-        var newBook = new Book(titleInput, authorInput, pagesInput);
+        var newBook = '';
+        if (bookRead == true) {
+            var newBook = new Book(titleInput, authorInput, pagesInput, 'Yes');
+        } else if (bookNotRead == true) {
+            var newBook = new Book(titleInput, authorInput, pagesInput, 'No');
+        }
 
         myLibrary.push(newBook);
 
@@ -37,9 +55,11 @@ function addBookToLibrary() {
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
         cell1.innerHTML = myLibrary[myLibrary.length - 1].bookTitle;
         cell2.innerHTML = myLibrary[myLibrary.length - 1].bookAuthor;
         cell3.innerHTML = myLibrary[myLibrary.length - 1].bookPages;
+        cell4.innerHTML = myLibrary[myLibrary.length - 1].bookRead;
 
         document.getElementById('book-title').value = '';
         document.getElementById('book-author').value = '';
